3baeb0cfc2321aebff521a4b67c6a7e3c37d50e8
Elisabete Martins de Oliveira <bete.mrt@gmail.com>
2022-10-12 09:46:14 -0300

    CSS quirto commit ShoppingCard

--- src/Pages/ShoppingCard.js
+++ src/Pages/ShoppingCard.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { getCategories, getProductsFromCategoryAndQuery, getProductById }
   from '../services/api';
-import './homePage.css';
+import './shoppingCard.css';
 
 class ShoppingCard extends Component {
   constructor() {
@@ -18,109 +18,130 @@ class ShoppingCard extends Component {
     };
   }
 
-  async componentDidMount() {
-    const categoriesArray = await getCategories();
-    this.setState({ categories: categoriesArray });
+  componentDidMount() {
+    this.getListItem();
   }
 
-  addValueInput = async ({ target }) => {
-    this.setState({ valueInput: target.value });
-  };
-
-  getProductsFromCategoryAndQueryAPI = async () => {
-    const { valueInput } = this.state;
-    this.setState({ stateCategory: false });
-    const researchReturn = await getProductsFromCategoryAndQuery(valueInput, valueInput);
-    if ((researchReturn === undefined) || (researchReturn.results.length === 0)) {
-      this.setState({ stateResearch: true });
-    } else {
+  getListItem = () => {
+    const itemString = localStorage.getItem('listCart');
+    if (itemString !== null) {
+      const itemBreak = JSON.parse(itemString);
+      const ids = itemBreak.map((item) => item.id);
+      const arrayProductsNoRepeat = ids
+        .filter((product, index, array) => array.indexOf(product) === index);
+      const arrayFiltered = arrayProductsNoRepeat.map((id) => (
+        itemBreak.filter((item) => item.id === id)
+      ));
       this.setState({
-        list: researchReturn.results,
-        stateResearch: false,
+        listShoppingCard: arrayFiltered,
       });
     }
   };
 
-  getProductsFromCategoryAP = async (id) => {
-    const { stateCategory } = this.state;
-    const productsFromCategory = await getProductById(id);
+  removeItem = ({ target: { name } }) => {
+    const { listShoppingCard } = this.state;
+    const id = name;
+    const allItems = listShoppingCard;
+    const newList = allItems.map((list) => list.filter((item) => item.id !== id));
+    const tiraArrayVazio = newList.filter((array) => array.length > 0);
+    localStorage.setItem('listCart', JSON.stringify(tiraArrayVazio));
     this.setState({
-      productsFromCategory: productsFromCategory.results,
-      stateCategory: !stateCategory,
+      listShoppingCard: tiraArrayVazio,
+    });
+  };
+
+  mudaQuantItem = ({ target: { name, value } }) => {
+    const { listShoppingCard } = this.state;
+    const id = name;
+    listShoppingCard.forEach((arrItem, ind, arr) => {
+      if (arrItem[0].id === id) {
+        if (value === 'sum' && arrItem[0].available_quantity > arrItem.length) {
+          arrItem.push(arrItem[0]);
+          this.setState({ listShoppingCard: arr });
+        } else if (value === 'sub' && arrItem.length > 1) {
+          arrItem.shift(arrItem[0]);
+          this.setState({ listShoppingCard: arr });
+        }
+      }
     });
   };
 
   render() {
-    const { list, categories, valueInput, stateResearch, productsFromCategory, stateCategory, listShoppingCard } = this.state;
+    const { listShoppingCard } = this.state;
     return (
-      <div className="containerGeral">
-        <header className="containerHeader">
-          <div className="containerSearch">
-            { list && (
-              <label
-                htmlFor="inputSearch"
-                data-testid="home-initial-message"
-                className="inputSearchLabel"
-              >
-                Digite algum termo de pesquisa ou escolha uma categoria.
-                <input
-                  type="text"
-                  data-testid="query-input"
-                  className="inputSearch"
-                  id="inputSearch"
-                  value={ valueInput }
-                  onChange={ this.addValueInput }
-                  placeholder="Digite algum termo de pesquisa ou escolha uma categoria."
-                />
-              </label>) }
-            <button
-              type="button"
-              data-testid="query-button"
-              className="btnSearch"
-              onClick={ this.getProductsFromCategoryAndQueryAPI }
-            >
-              Buscar
-            </button>
+      <div className="containerGeralSC">
+        <header className="containerHeaderSC">
+          <div>
+            <Link to="/">
+              <h1 className="titleHeaderSC">.</h1>
+            </Link>
           </div>
-          <div className="containerBtnCart">
+          <div className="containerBtnCartSC">
             <Link
               to="/shopping-card"
               data-testid="shopping-cart-button"
-              className="btnCart"
+              className="btnCartSC"
             />
           </div>
         </header>
-        <div className="containerCategories">
+        <div className="containerContentSC">
           <div>
-            <h1 className="titleHeader">F</h1>
-          </div>
-          <h3 className="categoriesTitle">Categorias</h3>
-          <div className="categoriesDiv">
-            <ul className="categoriesUl">
-              { categories.map((categorie) => (
-                <li key={ categorie.id }>
-                  <button
-                    type="button"
-                    data-testid="category"
-                    onClick={ () => this.getProductsFromCategoryAP(categorie.id) }
-                    className="categoriesLi"
-                  >
-                    { categorie.name }
-                  </button>
-                  <br />
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="containerContent">
-          <div>
-            { listShoppingCard.length === 0 && (
+            { listShoppingCard.length > 0 && (
+              <Link to="/checkout">
+                <button
+                  type="button"
+                  data-testid="checkout-products"
+                >
+                  Finalizar compra
+                </button>
+              </Link>
+            )}
+
+            { listShoppingCard.length > 0 ? listShoppingCard.map((item, ind) => (
+
+              <section
+                key={ ind }
+              >
+                <p data-testid="shopping-cart-product-name">{item[0].title}</p>
+                <img src={ item[0].thumbnail } alt={ item[0].title } />
+                <p>{item[0].price}</p>
+                <p data-testid="shopping-cart-product-quantity">
+                  { `Quantidade ${item.length}`}
+                </p>
+                <button
+                  type="button"
+                  data-testid="remove-product"
+                  onClick={ this.removeItem }
+                  name={ item[0].id }
+                >
+                  X
+                </button>
+                <button
+                  name={ item[0].id }
+                  type="button"
+                  value="sum"
+                  onClick={ this.mudaQuantItem }
+                  data-testid="product-increase-quantity"
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  name={ item[0].id }
+                  value="sub"
+                  onClick={ this.mudaQuantItem }
+                  data-testid="product-decrease-quantity"
+                >
+                  -
+                </button>
+              </section>
+            )) : (
               <p
                 data-testid="shopping-cart-empty-message"
+                className="shoppingCardEmptyMessageSC"
               >
                 Seu carrinho está vazio
-              </p>) }
+              </p>)}
           </div>
         </div>
       </div>
